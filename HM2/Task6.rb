1e1593978f2639abeb639fba0470c0f81ba9a369
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-06-29 13:07:39 +0400

    Task6
    some added to task6

--- HM2/Task6.rb
+++ HM2/Task6.rb
@@ -1,12 +1,12 @@
 def sort_string(my_string)
   unsorted = my_string.split.to_a
   sorted = []
-  (1..9).each {|x|
+  (1..unsorted.length).each {|x|
     unsorted.each {|word|
       sorted << word if word.include?(x.to_s)
     }
   }
   sorted.join(' ')
 end
-print sort_string("Hell3o m1y fri2end")
+print sort_string("Hell3o m1y fri2end wor8d wor4d wor7d wor5d wor9d wor6d ")
 
