14134f5f4e437891dc065752091dbfa500d40e9f
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-07-01 20:02:31 +0400

    HM3 and HM4
    ----

--- HM3/Task1.rb
+++ HM3/Task1.rb
@@ -1,2 +1,14 @@
+array = Array[[3, 5, 3, 2], [5, 7, 2, 9], [7, 5, 12, 19]]
+def sumofmin(array)
+  res = 0
+  array.each do
+    |array| res+= array.min
+  end
+  puts ('Sum of minimun = ' + res.to_s)
+end
+
+puts sumofmin(array)
+
+
 
 
