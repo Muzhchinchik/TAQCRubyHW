e475c86ebe307722e5c6e42e6a7882b41e862812
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-07-06 17:20:09 +0300

    HM5
    Task1

--- HM4/Task2.rb
+++ HM4/Task2.rb
@@ -4,12 +4,19 @@ def numbers(number)
   second = %w[десять одинадцать тринадцать четырнадцать пятнадцать шестнадцать семьнадцать восемьнадцать девятнадцать]
   third = %w[двадцать тридцать сорок пятьдесят шестьдесят семьдесят восемьдесят девяносто]
   if number > 0 && number <= 9
-    return first[number-1];
+    return first[number-1]
   end
-  if number >= 10 && number <= 20
-    return second[number-11];
+  if number > 9 && number <= 19 && s
+    return second[number%10]
   end
+  if number > 19 && number <= 99
+    return third[number/10-2] +' '+ first[number%10-1]
+
+  end
+
 end
 
-puts numbers(17)
+puts numbers(3)
 
+#else third[number/10 -2] +' '+ first[number % 10-1]
+#     end
---
+++ HM5/Task1.rb
@@ -0,0 +1,104 @@
+cars = [
+  {
+    :year => 2020,
+    "name" => "Skoda",
+    "model" => "Karoq",
+    :mileage => 10000,
+    :color => "blue"
+  },
+  {
+    :year => 2008,
+    "name" => "Kia",
+    "model" => "Ceed",
+    :mileage => 16000,
+    :color => "blue"
+  },
+  {
+    :year => 2013,
+    "name" => "Kia",
+    "model" => "Soul",
+    :mileage => 150000,
+    :color => "white"
+  },
+  {
+    :year => 1988,
+    "name" => "Peugeot",
+    "model" => "405",
+    :mileage => 320000,
+    :color => "blue"
+  },
+  {
+    :year => 2017,
+    "name" => "Nissan",
+    "model" => "Quashkai",
+    :mileage => 45000,
+    :color => "violet"
+  },
+  {
+    :year => 1972,
+    "name" => "ВАЗ",
+    "model" => "2103",
+    :mileage => 480000,
+    :color => "dark red"
+  },
+  {
+    :year => 2002,
+    "name" => "Nissan",
+    "model" => "X-Trail",
+    :mileage => 213000,
+    :color => "green"
+  }
+]
+def averageMileage
+
+end
+
+def averageYearOfProdaction
+
+end
+
+def averageYear
+
+end
+
+def averageMileagePerYear
+
+end
+
+def medianOfMileage
+
+end
+
+def medianYearOfProdaction
+
+end
+
+def medianYear
+
+end
+
+def medianMileagePerYear
+
+
+end
+
+def mostPopularColour(carr)
+
+end
+
+def theRarestColour
+
+end
+
+p mostPopularColour cars
+
+# Средний пробег
+# Средний год выпуска
+# Средний возраст
+# Средний пробег за год
+# Медиана пробега
+# Медиана года выпуска
+# Медиана возраста
+# Медиана пробега за год
+# Самый популярный цвет авто (массив, если их > 1)
+# Самый редкий цвет авто (массив, если их > 1)
