0def680fc187c9e24b26b00485131891e3f0dc24
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-08-16 21:02:09 +0300

    HM7
    Task1

--- HM6/Task1.rb
+++ HM6/Task1.rb
@@ -1,6 +1,6 @@
 #Написать функцию, которая преобразует названия cssстилей с дефисом в название в СamelСase стиле:
 # font-size в fontSize, background-color в backgroundColor, text-align в textAlign.
 
-str = "background-color"
+str = "background-color-qqq"
 print str.downcase.split("-").map.with_index{ |word, index|  index == 0 ? word : word[0].upcase() + word.slice(1..word.length)}.join('')
 
---
+++ HM7/Task1.rb
@@ -0,0 +1,14 @@
+#Напишите функцию, которая возвращает логин в виде строки случайной длины в диапазоне от 2 до 20 символов.
+#Первый символ - латинская буква верхнего регистра, остальные символы - латинские буквы нижнего регистра или цифры.
+
+length = rand(1..19)
+def random_string(length)
+  chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
+  firstLetter = 'ABCDEFGHJKLMNPQRSTUVWXYZ'
+  login = ''
+  first = ''
+  first << firstLetter[rand(firstLetter.size)]
+  length.times { login << chars[rand(chars.size)] }
+  first.concat(login)
+end
+puts random_string(length)
