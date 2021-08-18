65080a7a09e3d23d8090c75aa498ea2b08dd5d5f
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-08-18 21:03:51 +0300

    Task8
    SomethingTOsomething

--- HM8/Task1.rb
+++ HM8/Task1.rb
@@ -1,8 +1,27 @@
-f = c * 9/5 + 32
-k =
-  C F K
-ctoF = c * 9/5 + 32 #C -> F
-def somethingTOsomething(first, second, third)
+def somethingTOsomething(value, unit1, unit2)
 
+  sequence = ''
+  sequence.concat(unit1).concat(unit2)
+
+  case sequence
+  when 'cf'
+    someTOsome = value * 9/5 + 32
+  when 'fc'
+    someTOsome = (value - 32) * 5/9
+  when 'ck'
+    someTOsome = value + 273.15
+  when 'kc'
+    someTOsome = value - 273.15
+  when 'kf'
+    someTOsome = (value + 459.67) * 5/9
+  when 'fk'
+    someTOsome = (value - 32) / 1.8
+  else
+    someTOsome = 'Zhopa'
+  end
+
+  return someTOsome
 
 end
+
+puts somethingTOsomething(100, 'k1', 'f' )
